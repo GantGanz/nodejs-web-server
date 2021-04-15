e9b852e34561953c825cdf9364a5b540f07c1c2c
GantGanz <anggerelegant7@gmail.com>
2021-04-15 20:09:45 +0700

    response status

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const http = require('http');
 
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 200;
 
     const {
         method,
@@ -11,12 +10,15 @@ const requestListener = (request, response) => {
 
     if (url === '/') {
         if (method === 'GET') {
+            response.statusCode = 200;
             response.end('<h1>Ini adalah homepage</h1>');
         } else {
+            response.statusCode = 400;
             response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
         }
     } else if (url === '/about') {
         if (method === 'GET') {
+            response.statusCode = 200;
             response.end('<h1>Halo! Ini adalah halaman about</h1>')
         } else if (method === 'POST') {
             let body = [];
@@ -30,12 +32,15 @@ const requestListener = (request, response) => {
                 const {
                     name
                 } = JSON.parse(body);
+                response.statusCode = 200;
                 response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
             });
         } else {
+            response.statusCode = 400;
             response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
         }
     } else {
+        response.statusCode = 404;
         response.end('<h1>Halaman tidak ditemukan!</h1>');
     }
 };
